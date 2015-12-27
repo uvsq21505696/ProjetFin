112aa1d015a4590fdf779cc5c2b00a072fb2e379
uvsq21505696 <faladejoannes2@gmail.com>
2015-12-27 23:22:24 +0100

    deuxiemecommit

---
+++ ProjetGL/src/main/java/Joannes/App.java
@@ -0,0 +1,13 @@
+package Joannes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
